73cd9285b7868e1bfc5b93e868e1e7900803d88b
dwain <dwain@pluto.dwain.nl>
2020-02-11 18:33:17 +0100

    Version 1.1.0

---
+++ custom_components/dwains_theme/__init__.py
@@ -0,0 +1,110 @@
+# This is a fork from lovelace_gen
+# Many thanks to Thomasloven! :D
+
+import os
+import logging
+import json
+import io
+import time
+from collections import OrderedDict
+
+import jinja2
+
+from homeassistant.util.yaml import loader
+from homeassistant.exceptions import HomeAssistantError
+
+_LOGGER = logging.getLogger(__name__)
+
+def fromjson(value):
+    return json.loads(value)
+
+jinja = jinja2.Environment(loader=jinja2.FileSystemLoader("/"))
+
+jinja.filters['fromjson'] = fromjson
+
+dwains_theme_config = {}
+dwains_theme_translations = {}
+dwains_theme_icons = {}
+
+def load_yaml(fname, args={}):
+    try:
+        ll_gen = False
+        with open(fname, encoding="utf-8") as f:
+            if f.readline().lower().startswith("# dwains_theme"):
+                ll_gen = True
+
+        if ll_gen:
+            stream = io.StringIO(jinja.get_template(fname).render({
+                **args, 
+                "_d_t_config": dwains_theme_config, 
+                "_d_t_trans": dwains_theme_translations,
+                "_d_t_icons": dwains_theme_icons
+                }))
+            stream.name = fname
+            return loader.yaml.load(stream, Loader=loader.SafeLineLoader) or OrderedDict()
+        else:
+            with open(fname, encoding="utf-8") as config_file:
+                return loader.yaml.load(config_file, Loader=loader.SafeLineLoader) or OrderedDict()
+    except loader.yaml.YAMLError as exc:
+        _LOGGER.error(str(exc))
+        raise HomeAssistantError(exc)
+    except UnicodeDecodeError as exc:
+        _LOGGER.error("Unable to read file %s: %s", fname, exc)
+        raise HomeAssistantError(exc)
+
+
+def _include_yaml(ldr, node):
+    args = {}
+    if isinstance(node.value, str):
+        fn = node.value
+    else:
+        fn, args, *_ = ldr.construct_sequence(node)
+    fname = os.path.abspath(os.path.join(os.path.dirname(ldr.name), fn))
+    try:
+        return loader._add_reference(load_yaml(fname, args), ldr, node)
+    except FileNotFoundError as exc:
+        _LOGGER.error("Unable to include file %s: %s", fname, exc);
+        raise HomeAssistantError(exc)
+
+loader.load_yaml = load_yaml
+loader.yaml.SafeLoader.add_constructor("!include", _include_yaml)
+
+async def async_setup(hass, config):
+    #Load main config
+    dwains_theme_config.update(config.get("dwains_theme")["configuration"]);
+
+    #Load translations
+    language = dwains_theme_config["global"]["language"];
+    dwains_theme_translations.update(config.get("dwains_theme")["translations"][language]);
+
+    #Load icons
+    if ("icons" in config.get("dwains_theme")):
+        icons_config = config.get("dwains_theme")["icons"]["icons"];
+        dwains_theme_icons.update(icons_config);
+
+    return True
+
+# Allow redefinition of node anchors
+import yaml
+
+def compose_node(self, parent, index):
+    if self.check_event(yaml.events.AliasEvent):
+        event = self.get_event()
+        anchor = event.anchor
+        if anchor not in self.anchors:
+            raise yaml.composer.ComposerError(None, None, "found undefined alias %r"
+                    % anchor, event.start_mark)
+        return self.anchors[anchor]
+    event = self.peek_event()
+    anchor = event.anchor
+    self.descend_resolver(parent, index)
+    if self.check_event(yaml.events.ScalarEvent):
+        node = self.compose_scalar_node(anchor)
+    elif self.check_event(yaml.events.SequenceStartEvent):
+        node = self.compose_sequence_node(anchor)
+    elif self.check_event(yaml.events.MappingStartEvent):
+        node = self.compose_mapping_node(anchor)
+    self.ascend_resolver()
+    return node
+
+yaml.composer.Composer.compose_node = compose_node
