b5245c9db7a279e8a9fc5af8b1cc5ea19bd8d61c
billygr <billygr@users.noreply.github.com>
2025-03-15 18:44:37 +0200

    Fixes this warning (no test case to test the functionality) Detected code that calls async_forward_entry_setup for integration dwains_dashboard with title: and entry_id: 90f02e0845a71753189cbec2822f5bb9, during setup without awaiting async_forward_entry_setup, which can cause the setup lock to be released before the setup is done. This will stop working in Home Assistant 2025.1, please report this issue

--- custom_components/dwains_dashboard/__init__.py
+++ custom_components/dwains_dashboard/__init__.py
@@ -1587,11 +1587,7 @@ async def async_setup_entry(hass, config_entry):
 
     config_entry.add_update_listener(_update_listener) 
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(
-            config_entry, "sensor"
-        )
-    )
+    await hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
 
     return True
 
